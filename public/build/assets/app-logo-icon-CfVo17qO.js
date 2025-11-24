182c88f762c33a76f68ceef7dcae059ebef9603c
KeviinDCV <110347632+KeviinDCV@users.noreply.github.com>
2025-11-24 10:36:07 -0500

    fx
    Update asset bundle hashes after build

--- public/build/assets/app-logo-icon-CfVo17qO.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-D99SyvRR.js";function n({size:o=36,className:t,...g}){return r.jsx("img",{src:"/images/logo.png",alt:"HERMES Logo",width:o,height:o,className:t,...g})}export{n as A};
---
+++ public/build/assets/app-logo-icon-CvRqHJxV.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-kB0rY2ZU.js";function n({size:o=36,className:t,...g}){return r.jsx("img",{src:"/images/logo.png",alt:"HERMES Logo",width:o,height:o,className:t,...g})}export{n as A};
